eb278f3e3a95e2641bf2cfb82414fdfc9066e933
mahmoud mousa <mahmoudmostafa194@gmail.com>
2020-11-11 21:47:45 +0200

    Score setup

---
+++ Assets/_Scripts/Score.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    public int score=0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Collectable"))
+        {
+            score++;
+            other.gameObject.SetActive(false);
+        }
+    }
+}
